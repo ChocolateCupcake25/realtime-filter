d43cd2f8392bdf2c121f91321cbf3b82fa11e58b
ChocolateCupcake25 <92889263+ChocolateCupcake25@users.noreply.github.com>
2021-12-17 10:27:41 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+mustache=loadImage(' https://i.postimg.cc/3x3QzSGq/m.png');
 }
  function setup(){
      canvas= createCanvas(300,350);
@@ -19,11 +21,18 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x = "+ results[0].pose.nose.x);
-        console.log("nose y = "+ results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x = "+ noseX);
+        console.log("nose y = "+ noseY);
 
   }}
 
  function draw(){
-    image(video,0,0,300,300);
+    image(video,0,0,300,350);
+    image(mustache,noseX-25,noseY,50,50);
+ }
+
+ function take_Snapshot(){
+     save("Image.png")
  }
